b0827249df99a505f829cd0ee9ace66269da765b
Noktomezo <noktomezo@duck.com>
2025-05-05 19:24:59 +0300

    chore: stategy fix

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -10,5 +10,5 @@ $WINWS_ARGS = "`
 --filter-tcp=443 --hostlist=`"$HOSTS\russia-blocked.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new `
 --filter-tcp=443 --hostlist-auto=`"$HOSTS\autohostlist.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new `
 --filter-udp=443 --hostlist=`"$HOSTS\russia-blocked.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$FILES\quic_initial_www_google_com.bin`" --new `
---filter-udp=443 --hostlist=`"$HOSTS\autohostlist.txt`" --dpi-desync-fake-quic=`"$FILES\quic_initial_www_google_com.bin`" --new `
+--filter-udp=443 --hostlist=`"$HOSTS\autohostlist.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$FILES\quic_initial_www_google_com.bin`" --new `
 --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake"
