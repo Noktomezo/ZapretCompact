ee3d9eee255f76092660f31a319f5f5ea3350311
Noktomezo <noktomezo@duck.com>
2025-05-16 19:03:20 +0300

    chore: update strategy for discord

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -1,9 +1,10 @@
 $ROOT = Split-Path -Parent $PSScriptROOT
 $FILES = Join-Path $ROOT 'files'
-$HOSTS = Join-Path $ROOT 'hosts'
+$DOMAINS = Join-Path $ROOT 'hosts'
 
-$AUTOHOSTLIST = Join-Path $HOSTS 'autohostlist.txt'
-$RUSSIA_BLOCKED_LIST = Join-Path $HOSTS 'russia-blocked.txt'
+$AUTOHOSTLIST = Join-Path $DOMAINS 'autohostlist.txt'
+$RUSSIA_BLOCKED_LIST = Join-Path $DOMAINS 'russia-blocked.txt'
+$DISCORD_LIST = Join-Path $DOMAINS 'discord.txt'
 $QUIC_BIN = Join-Path $FILES 'quic_initial_www_google_com.bin'
 
 return @{
@@ -11,10 +12,11 @@ return @{
   NSSM_EXE   = Join-Path $FILES 'nssm.exe'
   WINWS_EXE  = Join-Path $FILES 'winws.exe'
 
-  # Аргументы командной строки
+  # Стратегии
   WINWS_ARGS = @(
     "--wf-tcp=80,443 --wf-udp=443,50000-50099",
     "--filter-tcp=80 --hostlist-auto=`"$AUTOHOSTLIST`" --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new",
+    "--filter-tcp=443 --hostlist=`"$DISCORD_LIST`" --dpi-desync=fake,split --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.cloudflare.com --new",
     "--filter-tcp=443 --hostlist=`"$RUSSIA_BLOCKED_LIST`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new",
     "--filter-tcp=443 --hostlist-auto=`"$AUTOHOSTLIST`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --new",
     "--filter-udp=443 --hostlist=`"$RUSSIA_BLOCKED_LIST`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$QUIC_BIN`" --new",
