93f63ad94cf792737787fb2902a387d1fd1827b5
Noktomezo <noktomezo@duck.com>
2025-10-24 00:49:30 +0300

    chore: switch to different strategy

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -17,7 +17,7 @@ return @{
   WINWS_ARGS = @(
     "--wf-tcp=80,443 --wf-udp=443,50000-50099",
     "--filter-tcp=80 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multisplit --dpi-desync-fooling=md5sig --dpi-desync-autottl=1",
-    "--new --filter-tcp=443 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=split2 --dpi-desync-split-seqovl=681 --dpi-desync-split-seqovl-pattern=`"$GOOGLE_TLS_BIN`"",
+    "--new --filter-tcp=443 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multidisorder --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=2 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com",
     "--new --filter-udp=443 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig,badseq --dpi-desync-fake-quic=`"$GOOGLE_QUIC_BIN`"",
     "--new --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord=0x00 --dpi-desync-fake-stun=0x00"
   ) -Join " "
