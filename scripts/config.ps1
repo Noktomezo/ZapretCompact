43794546b9e14e494786d8a31feefd7bb1ca3a7c
Noktomezo <noktomezo@duck.com>
2025-11-11 21:32:12 +0300

    chore: update strategies

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -2,11 +2,12 @@ $ROOT = Split-Path -Parent $PSScriptROOT
 $FILES = Join-Path $ROOT 'files'
 $DOMAINS = Join-Path $ROOT 'hosts'
 
-$RUSSIA_BLOCKED_IPSET = Join-Path $DOMAINS 'russia-blocked-ipset.txt'
-# $YOUTUBE_LIST = Join-Path $DOMAINS 'youtube.txt'
+$IPSET_RUSSIA_BLOCKED = Join-Path $DOMAINS 'ipset-russia-blocked.txt'
+$IPSET_AMAZON = Join-Path $DOMAINS 'ipset-amazon.txt'
+$HOSTS_GOOGLE = Join-Path $DOMAINS 'hosts-google.txt'
+
 $GOOGLE_QUIC_BIN = Join-Path $FILES 'quic_initial_www_google_com.bin'
 $GOOGLE_TLS_BIN = Join-Path $FILES 'tls_clienthello_www_google_com.bin'
-# $IANA_TLS_BIN = Join-Path $FILES 'tls_clienthello_iana_org.bin'
 
 return @{
   # Экзешники
@@ -15,10 +16,14 @@ return @{
 
   # Стратегии
   WINWS_ARGS = @(
-    "--wf-tcp=80,443 --wf-udp=443,50000-50099",
-    "--filter-tcp=80 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multisplit --dpi-desync-fooling=md5sig --dpi-desync-autottl=1",
-    "--new --filter-tcp=443 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multidisorder --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=2 --dpi-desync-fooling=badseq --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com",
-    "--new --filter-udp=443 --ipset=`"$RUSSIA_BLOCKED_IPSET`" --dpi-desync=fake,multidisorder --dpi-desync-fooling=md5sig,badseq --dpi-desync-fake-quic=`"$GOOGLE_QUIC_BIN`"",
-    "--new --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord=0x00 --dpi-desync-fake-stun=0x00"
+    "--wf-tcp=80,443,20000-21999 --wf-udp=443,19294-19344,20000-21999,50000-50100",
+    "--filter-tcp=80 --ipset=`"$IPSET_RUSSIA_BLOCKED`" --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=badsum",
+    "--new --filter-tcp=443 --hostlist=`"$HOSTS_GOOGLE`" --dpi-desync=split2 --dpi-desync-split-seqovl=681 --dpi-desync-split-seqovl-pattern=`"$GOOGLE_TLS_BIN`"",
+    "--new --filter-tcp=443 --ipset=`"$IPSET_RUSSIA_BLOCKED`" --dpi-desync=multidisorder --dpi-desync-split-pos=1,sniext+1,host+1,midsld-2,midsld,midsld+2,endhost-1",
+    "--new --filter-tcp=20000-21999 --ipset=`"$IPSET_AMAZON`" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=10000000",
+    "--new --filter-udp=443 --hostlist=`"$HOSTS_GOOGLE`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$GOOGLE_QUIC_BIN`"",
+    "--new --filter-udp=443 --ipset=`"$IPSET_RUSSIA_BLOCKED`" --dpi-desync=fake --dpi-desync-repeats=11",
+    "--new --filter-udp=20000-21999 --ipset=`"$IPSET_AMAZON`" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp=`"$GOOGLE_QUIC_BIN`" --dpi-desync-cutoff=n2",
+    "--new --filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake"
   ) -Join " "
 }
